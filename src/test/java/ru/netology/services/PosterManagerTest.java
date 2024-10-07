b6a48d1d80b7d8191f7eb1df20f5d630a2cb2448
Roman Leshin <vorzee@gmail.com>
2024-10-07 23:51:55 +0500

    Добавлена проверка содержимого ответа "Протестированы должны быть как минимум следующие интересные случаи для метода получения последних фильмов: когда в менеджере фильмов меньше чем лимит, когда больше и когда столько же." Метод `testGetLatestCustom` делал аналогичную проверку, но только по размеру выдачи

--- src/test/java/ru/netology/services/PosterManagerTest.java
+++ src/test/java/ru/netology/services/PosterManagerTest.java
@@ -89,5 +89,55 @@ class PosterManagerTest {
         int actualCount = actualPosters.length;
         Assertions.assertEquals(expectedCount, actualCount);
     }
+
+
+    @Test
+    void shouldReturnAllIfLessThanLimit() {
+        setUpValues(10);
+        Poster[] actualPosters = posterManager.findLast();
+
+        Poster[] expectedPosters = {
+                new Poster("The Matrix", "A computer hacker learns from mysterious rebels about", "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg", "2010"),
+                new Poster("The Lord of the Rings: The Return of the King", "Gandalf and Aragorn lead the World of Men against Sauron's army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring.", "https://m.media-amazon.com/images/M/MV5BNzA5ZDNkZDQtNDg0NS00ODQzLTg0ZmItYjYzZDQ1Yjk0ODNlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg", "2003"),
+                new Poster("Inception", "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.", "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg", "2010"),
+                new Poster("The Dark Knight", "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.", "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg", "2008"),
+                new Poster("The Godfather", "An organized crime dynasty's aging patriarch, now in his late 70s, calls in a protege from out of town to help his son take control of the family business.", "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtY2FiMS00N2YwLWE5ZjctYjBkMThlMjM5ZGIwXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg", "1972"),
+                new Poster("The Shawshank Redemption", "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.", "https://m.media-amazon.com/images/M/MV5BNDE3ODAzMjcxNF5BMl5BanBnXkFtZTcwNTg0NDY4OQ@@._V1_SX300.jpg", "1994")
+        };
+
+        Assertions.assertArrayEquals(expectedPosters, actualPosters);
+    }
+
+    @Test
+    void shouldReturnLimitedNumberIfMoreThanLimit() {
+        setUpValues(4);
+        Poster[] actualPosters = posterManager.findLast();
+
+        Poster[] expectedPosters = {
+                new Poster("The Matrix", "A computer hacker learns from mysterious rebels about", "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg", "2010"),
+                new Poster("The Lord of the Rings: The Return of the King", "Gandalf and Aragorn lead the World of Men against Sauron's army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring.", "https://m.media-amazon.com/images/M/MV5BNzA5ZDNkZDQtNDg0NS00ODQzLTg0ZmItYjYzZDQ1Yjk0ODNlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg", "2003"),
+                new Poster("Inception", "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.", "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg", "2010"),
+                new Poster("The Dark Knight", "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.", "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg", "2008")
+        };
+
+        Assertions.assertArrayEquals(expectedPosters, actualPosters);
+    }
+
+    @Test
+    void shouldReturnExactNumberIfEqualToLimit() {
+        setUpValues(6);
+        Poster[] actualPosters = posterManager.findLast();
+
+        Poster[] expectedPosters = {
+                new Poster("The Matrix", "A computer hacker learns from mysterious rebels about", "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg", "2010"),
+                new Poster("The Lord of the Rings: The Return of the King", "Gandalf and Aragorn lead the World of Men against Sauron's army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring.", "https://m.media-amazon.com/images/M/MV5BNzA5ZDNkZDQtNDg0NS00ODQzLTg0ZmItYjYzZDQ1Yjk0ODNlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg", "2003"),
+                new Poster("Inception", "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.", "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg", "2010"),
+                new Poster("The Dark Knight", "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.", "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg", "2008"),
+                new Poster("The Godfather", "An organized crime dynasty's aging patriarch, now in his late 70s, calls in a protege from out of town to help his son take control of the family business.", "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtY2FiMS00N2YwLWE5ZjctYjBkMThlMjM5ZGIwXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg", "1972"),
+                new Poster("The Shawshank Redemption", "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.", "https://m.media-amazon.com/images/M/MV5BNDE3ODAzMjcxNF5BMl5BanBnXkFtZTcwNTg0NDY4OQ@@._V1_SX300.jpg", "1994")
+        };
+
+        Assertions.assertArrayEquals(expectedPosters, actualPosters);
+    }
 }
 
